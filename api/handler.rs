e58ecf1572cd8cbbad4bb32a4f23b6ac28a1ba3f
aliuahma <hiimaliahmad@gmail.com>
2025-01-08 03:02:11 -0800

    process template through tera

--- api/handler.rs
+++ api/handler.rs
@@ -1,5 +1,5 @@
-use std::include_str;
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
+use tera::{Tera, Context};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -7,10 +7,14 @@ async fn main() -> Result<(), Error> {
 }
 
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
-    let html = include_str!("../templates/base.html");
+    let mut tera = Tera::new("templates/**/*").map_err(|e| Error::from(e.to_string()))?;
+    let context = Context::new();
+    
+    let rendered = tera.render("home.html", &context)
+        .map_err(|e| Error::from(e.to_string()))?;
     
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "text/html")
-        .body(html.into())?)
+        .body(rendered.into())?)
 }
