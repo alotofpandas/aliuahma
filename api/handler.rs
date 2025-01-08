3189e264c95163726e13b51ee6306d08086ced2c
aliuahma <hiimaliahmad@gmail.com>
2025-01-08 02:48:40 -0800

    AHHHH

--- api/handler.rs
+++ api/handler.rs
@@ -7,21 +7,29 @@ async fn main() -> Result<(), Error> {
 }
 
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
-    let tera = Tera::new("templates/**/*")?;
+    let tera = match Tera::new("templates/**/*") {
+        Ok(t) => t,
+        Err(_) => return Ok(Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body("Template error".into())?)
+    };
+    
     let context = Context::new();
-
+    
     match req.uri().path() {
         "/" => {
-            let rendered = tera.render("home.html", &context)?;
-            Ok(Response::builder()
-                .status(StatusCode::OK)
-                .header("Content-Type", "text/html")
-                .body(rendered.into())?)
-        }
-        _ => {
-            Ok(Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body("Not Found".into())?)
-        }
+            match tera.render("home.html", &context) {
+                Ok(rendered) => Ok(Response::builder()
+                    .status(StatusCode::OK)
+                    .header("Content-Type", "text/html")
+                    .body(rendered.into())?),
+                Err(_) => Ok(Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body("Rendering error".into())?)
+            }
+        },
+        _ => Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body("Not Found".into())?)
     }
 }
